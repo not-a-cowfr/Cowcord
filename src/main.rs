37bdae63e4a6cb329635605225c71962e57c7908
Not a cow <kly3333aswd@gmail.com>
2025-04-02 18:22:16 -0700

    implement ssg i think

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,33 @@ pub enum Route {
 
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
-fn main() { dioxus::launch(App); }
+fn main() {
+	dioxus::LaunchBuilder::new()
+		.with_cfg(server_only! {
+			ServeConfig::builder()
+				.incremental(
+					IncrementalRendererConfig::new()
+						.static_dir(
+							std::env::current_exe()
+								.unwrap()
+								.parent()
+								.unwrap()
+								.join("dist")
+						)
+						.clear_cache(false)
+				)
+				.enable_out_of_order_streaming()
+		})
+		.launch(App);
+}
+
+#[server(endpoint = "static_routes")]
+async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+	Ok(Route::static_routes()
+		.iter()
+		.map(ToString::to_string)
+		.collect())
+}
 
 #[component]
 fn App() -> Element {
